1ba9f6a04c6cf68858fd43db39249e5f9dbf3405
abnersc045-ctrl <abnersc045@gmail.com>
2026-07-30 19:42:32 -0600

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-const facts=['Chocolate was once used as money by the Aztecs.','Honey never spoils.','Avocados are fruits.','Vanilla comes from orchids.','Nachos were invented in Mexico in 1943.'];document.getElementById('factBtn').onclick=()=>{factBox.innerHTML='🥠 <b>Your fortune says:</b><br><br>'+facts[Math.floor(Math.random()*facts.length)]};
